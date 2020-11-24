84597cfea26fc0b3bbe87e57a22caf12d4bba5ff
umang <25umangagarwal@gmail.com>
2020-11-24 23:26:58 +0530

    Added the laoder

---
+++ wildfire-tracker/src/Components/Loader.js
@@ -0,0 +1,11 @@
+import spinner from './Spinner.gif'
+
+const Loader= ()=> {
+    return (
+        <div>
+           <img src={spinner} alt="loading....."></img>
+        </div>
+    )
+}
+
+export default Loader
