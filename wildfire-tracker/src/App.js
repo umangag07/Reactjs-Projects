1641b4c4de3bd69e9ed34d9db943c56bf6daa702
umang <25umangagarwal@gmail.com>
2020-11-24 21:13:32 +0530

    Removed unecessary files and imported GoogleMapReact

---
+++ wildfire-tracker/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div>
+    <h1>React wildfire tracker</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ wildfire-tracker/src/Components/Map.js
@@ -0,0 +1,11 @@
+import GoogleMapReact from 'google-map-react'
+
+const Map=()=> {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Map
