e13cdbbdd3b41904768bb491c22b1f027150b9f9
umang <25umangagarwal@gmail.com>
2020-12-04 23:42:23 +0530

    Added the data for the services

---
+++ techo_service-website/src/Servicedata.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import MobileApp from '../src/images/Mobile_application.svg'
+const Servicedata=[
+    { imgsrc:MobileApp,
+      title:"Mobile App Development",
+      text:"You will get the app developed from finest developers." 
+    }, 
+    { imgsrc:MobileApp,
+      title:"Mobile App Development",
+      text:"You will get the app developed from finest developers." 
+    }, 
+    { imgsrc:MobileApp,
+      title:"Mobile App Development",
+      text:"You will get the app developed from finest developers." 
+    }, 
+    { imgsrc:MobileApp,
+      title:"Mobile App Development",
+      text:"You will get the app developed from finest developers." 
+    }, 
+    { imgsrc:MobileApp,
+      title:"Mobile App Development",
+      text:"You will get the app developed from finest developers." 
+    }, 
+    { imgsrc:MobileApp,
+      title:"Mobile App Development",
+      text:"You will get the app developed from finest developers." 
+    } 
+
+]
+
+export default Servicedata
