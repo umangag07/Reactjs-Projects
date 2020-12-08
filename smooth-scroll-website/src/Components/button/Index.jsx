fa879a75b462ae0a3f379a973d9b0682798e9e44
umang <25umangagarwal@gmail.com>
2020-12-09 02:42:52 +0530

    added the dependency

--- smooth-scroll-website/src/Components/button/Index.jsx
+++ smooth-scroll-website/src/Components/button/Index.jsx
@@ -3,12 +3,21 @@ import styled from 'styled-components'
 import { theme } from '../../styles';
 
 const ButtonWrapper = styled.button`
+    height:40px;
     padding:5px 12px;
-    border-radius:4px;
+    border-radius:6px;
     border:3px solid ${theme.color2}; 
     background-color:${theme.color2} ;
     color:${theme.color1};
     font-size:16px;
+    transition: all 200ms ease-in-out;
+    cursor:pointer;
+    &:hover{
+        background-color:transparent;
+        border:2px solid ${theme.color2};
+        color:#fff;
+        
+    }
 `;
 
 
