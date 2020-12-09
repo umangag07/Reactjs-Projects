37bbe7b76c9ab27faa0e63bcd1599a77c458a368
umang <25umangagarwal@gmail.com>
2020-12-09 12:55:01 +0530

    addded the buttons

--- smooth-scroll-website/src/Components/Navbar/Navbar.jsx
+++ smooth-scroll-website/src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Button } from '../button/Index';
 import { Indexlogo } from '../Logo/Indexlogo';
 
 const NavbarConatiner = styled.div`
@@ -9,23 +10,27 @@ const NavbarConatiner = styled.div`
    background-color:rgba(55,89,90,0.35);
    border-radius:30px;
    display:flex;
-   
+   justify-content: space-around;
 `;
 
 const BrandConatiner = styled.div`
     display:flex;
     align-items:center;
+    flex:1;
 `;
 
 const ButtonsConatiner = styled.div`
-
+   display:flex;
+   justify-content: flex-end;
+   margin:10px;
 `; 
 
 export function Navbar(props){
   return(
       <NavbarConatiner>
           <BrandConatiner><Indexlogo inline/></BrandConatiner>
-          <ButtonsConatiner></ButtonsConatiner>
+          <ButtonsConatiner><Button size="13.5" height="30" padding="2px 14px">Get Started</Button></ButtonsConatiner>
+          <ButtonsConatiner><Button size="13.5" height="30" padding="2px 19px">Login</Button></ButtonsConatiner>
       </NavbarConatiner>
   )
 }
