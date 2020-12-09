e7f0c8e4c4174d9eb005f31097f9cac672fd184b
umang <25umangagarwal@gmail.com>
2020-12-09 12:02:06 +0530

    Added the arrow component

---
+++ smooth-scroll-website/src/Components/Arrow/ArrowIndex.jsx
@@ -0,0 +1,36 @@
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import styled from "styled-components";
+import { theme } from '../../styles';
+
+const ArrowConatiner = styled.div`
+  width: 56px;
+  height: 56px;
+  border-radius: 50%;
+  background-color: rgba(155,189,190,0.45);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  border:15px solid transparent;
+  transition:all 500ms ease-in-out;
+  &:hover{
+      border:2px solid ${theme.color2}
+  }
+`;
+
+const ArrowIcon = styled.div`
+  color: #fff;
+  font-size: 35px;
+  
+`;
+export function Arrow(props) {
+  return (
+    <ArrowConatiner>
+      <ArrowIcon>
+        <FontAwesomeIcon icon={faAngleDown} />
+      </ArrowIcon>
+    </ArrowConatiner>
+  );
+}
