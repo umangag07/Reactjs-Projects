6723883ea5d31a92b21f8b18e718ae11882198f8
umang <25umangagarwal@gmail.com>
2020-12-10 12:13:19 +0530

    Fixed the positions of card

--- smooth-scroll-website/src/Components/ReviewCard/CardIndex.jsx
+++ smooth-scroll-website/src/Components/ReviewCard/CardIndex.jsx
@@ -7,8 +7,9 @@ const ReviewCardConatainer = styled.div`
     width:370px;
     height:500px;
     background-color:${theme.color1};
-    box-shadow: 0px 0px 16px rgba(88, 80, 80, 0.3);
+    box-shadow: 0px 0px 10px rgba(88, 80, 80, 0.3);
     border-radius:5px;
+    margin: 5px 2em;
 `;
 
 export function CardIndex() {
--- smooth-scroll-website/src/Container/homepage/ReviewSection.jsx
+++ smooth-scroll-website/src/Container/homepage/ReviewSection.jsx
@@ -1,19 +1,50 @@
-import { ButtonBack, ButtonNext, CarouselProvider, Slide, Slider } from "pure-react-carousel";
+import {
+  ButtonBack,
+  ButtonNext,
+  CarouselProvider,
+  DotGroup,
+  Slide,
+  Slider,
+} from "pure-react-carousel";
 import React from "react";
 import { Element } from "react-scroll";
 import styled from "styled-components";
 import { Marginer } from "../../Components/Marginer/Index";
 import { CardIndex } from "../../Components/ReviewCard/CardIndex";
 import { SectionTitle } from "../../Components/SectionTitle/index_sectionTitle";
-import 'pure-react-carousel/dist/react-carousel.es.css';
+import "pure-react-carousel/dist/react-carousel.es.css";
+import { theme } from "../../styles";
 const ReviewContainer = styled(Element)`
   min-height: 700px;
   display: flex;
-  flex-direction: column;;
-  align-items:center;
+  flex-direction: column;
+  align-items: center;
+  margin-left: 10vh;
+  margin-right: 10vh;
 `;
 const StyledCarousalProvider = styled(CarouselProvider)`
-    width:50%;
+  width: 70%; 
+`;
+const StyledSlide = styled(Slide)`
+  width: 70%;
+`;
+const StyledDotGroup = styled(DotGroup)`
+  button {
+    width: 2px;
+    height: 5px;
+    border-radius: 50%;
+    outline: none;
+    margin-top: 10px;
+    border: 0px;
+    background-color: #c4c4c4;
+    &:not(:last-of-type) {
+      margin-right: 3px;
+    }
+  }
+  .carousel__dot--selected {
+    background-color: rgb(53, 53, 53);
+    justify-content:center;
+  }
 `;
 
 export function ReviewSection() {
@@ -22,18 +53,27 @@ export function ReviewSection() {
       <SectionTitle>Reviews About Us</SectionTitle>
       <Marginer direction="vertical" margin="4em" />
       <StyledCarousalProvider
-       naturalSlideHeight={300}
-       naturalSlideWidth={200}
-       totalSlides={3}
-       visibleSlides={1}>
-      <Slider>
-          <Slide index={0}><CardIndex/></Slide>
-          <Slide index={1}><CardIndex/></Slide>
-          <Slide index={2}><CardIndex/></Slide>
-      </Slider>
-     
+        naturalSlideHeight={230}
+        naturalSlideWidth={200}
+        totalSlides={4}
+        visibleSlides={2}
+      >
+        <Slider>
+          <Slide index={0}>
+            <CardIndex />
+          </Slide>
+          <Slide index={1}>
+            <CardIndex />
+          </Slide>
+          <Slide index={2}>
+            <CardIndex />
+          </Slide>
+          <Slide index={3}>
+            <CardIndex />
+          </Slide>
+        </Slider>
+        <StyledDotGroup />
       </StyledCarousalProvider>
-     
     </ReviewContainer>
   );
 }
