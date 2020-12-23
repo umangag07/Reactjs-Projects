66fcfef5e67ea12bfbe186e054fc5bf66ba3fb2d
umang <25umangagarwal@gmail.com>
2020-12-23 18:11:32 +0530

    Added marginer and homepage section

---
+++ generalservicing_multipage_website/src/Containers/Index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const PageConatiner = styled.span`
+   display:flex;
+   width:100%auto;
+   height:100vh;
+   background-color:beige;
+`;
+function HomePage() {
+    return (
+        <>
+        <PageConatiner>
+
+        </PageConatiner>
+        </>
+    )
+}
+
+export default HomePage
