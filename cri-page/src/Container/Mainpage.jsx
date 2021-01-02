5dc2ac75601d225ea1196e087851813ea9319524
umang <25umangagarwal@gmail.com>
2021-01-03 02:07:56 +0530

    Made new customimage comp

--- cri-page/src/Container/Mainpage.jsx
+++ cri-page/src/Container/Mainpage.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
 import logo from '../Assets/logo.png'
+import CustomImager from '../Components/CustomImager'
 const PageContainer = styled.div`
   width:100%;
-  min-height:100vh;
+  min-height:200vh;
   background: rgb(255,255,174);
   display:flex;
   flex-direction:column;
-background: linear-gradient(0deg, rgba(255,255,174,1) 23%, rgba(255,252,222,1) 69%); 
+  background: rgb(255,255,174);
+  background: linear-gradient(0deg, rgba(255,255,174,1) 57%, rgba(255,252,222,1) 74%); 
 `
 const LogoContainer = styled.div`
     width:100%;
@@ -17,21 +19,21 @@ const LogoContainer = styled.div`
     justify-content:space-around;
     align-items:center;
 `
-const Logo = styled.div`
-    width:20em;
-    height:7.5em;
-    img{
-        width:100%;
-        height:100%;
-    }  
+
+const AwardContainer = styled.div`
+   width:100%;
+   height:89vh;
+   display:flex;
+   background-color:blueviolet;
 `
 function Mainpage() {
     return (
         <>
            <PageContainer>
                <LogoContainer>
-                   <Logo><img src={logo} alt="Icon"/></Logo>
+                 <CustomImager width = "20em" height = "7.5em" imgsrc={logo}/>
                </LogoContainer>
+               <AwardContainer></AwardContainer>
            </PageContainer> 
         </>
     )
